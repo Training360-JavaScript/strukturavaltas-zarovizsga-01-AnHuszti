dfb0a6a600bad95906587d4d2a22edbe32985c51
AnHuszti <husztiandrea82@gmail.com>
2022-02-28 16:44:23 +0100

    ...

--- angular-3/src/app/model/construction.ts
+++ angular-3/src/app/model/construction.ts
@@ -1,7 +1,7 @@
 //{"id":1,"material":"Plastic","trade":"Carpenter","cost_code":"1-550 - Vehicular Access and Parking","price":2015,"active":true}
 
 export class Construction {
-    [key: string]: any,
+    [key: string]: any;
     id: number = 0;
     material: string = '';
     trade: string = '';
